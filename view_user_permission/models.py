76c089802c0e4431ea9aeddcbac6b8be6bd098d2
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 10:33:45 +0430

    models of vup added

--- view_user_permission/models.py
+++ view_user_permission/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+action_map = (
+    ('retrieve','GET'),
+    ('create','POST'),
+    ('update','PUT'),
+    ('partial_update','PATCH'),
+    ('destroy','DELETE'),
+)
+class Permission(models.Model):
+    view = models.CharField(max_length=128)
+    action = models.SmallIntegerField(choices=action_map)
+
+class Group(models.Model):
+    permissions = models.ManyToManyField(Permission, related_name="permissions")
+    user = models.ManyToManyField(User, related_name="consist")
