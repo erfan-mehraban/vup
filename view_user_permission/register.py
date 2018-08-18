45de93d10d653324ef67ecd8d0e9878af1ea22d7
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 13:57:24 +0430

    permission classes tuple handling

--- view_user_permission/register.py
+++ view_user_permission/register.py
@@ -5,6 +5,8 @@ def register_view(view_class):
     """
     register view in registered_views list and adding vup permission to view permission_classes
     """
+    if isinstance(view_class.permission_classes, tuple):
+        raise ValueError(f"permission_classes of {view_class.__name__} must be list")
     view_class.permission_classes.append(UserPermission)
     registered_views.append(get_view_name(view_class))
     return view_class
