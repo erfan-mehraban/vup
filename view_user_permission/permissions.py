2940188201134d003343d34c93a23b11c78bc323
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 12:57:41 +0430

    including app name when saving view name

--- view_user_permission/permissions.py
+++ view_user_permission/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
 from .models import Permission
+from . import register
 
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.roles.filter(
-            permissions__name=Permission.get_name(view.__class__.__name__, view.action)
+            permissions__name=Permission.get_name(register.get_view_name(view.__class__), view.action)
             ).exists()
--- view_user_permission/register.py
+++ view_user_permission/register.py
@@ -6,5 +6,9 @@ def register_view(view_class):
     register view in registered_views list and adding vup permission to view permission_classes
     """
     view_class.permission_classes.append(UserPermission)
-    registered_views.append(view_class.__name__)
+    registered_views.append(get_view_name(view_class))
     return view_class
+
+def get_view_name(view_class):
+    """ combine app name and view name """
+    return view_class.__module__+"."+view_class.__name__
