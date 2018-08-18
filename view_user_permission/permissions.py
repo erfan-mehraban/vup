3c81a002cf01f304f0dc9d9af012432590c84af4
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 14:13:26 +0430

    athenticating required!

--- view_user_permission/permissions.py
+++ view_user_permission/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework import permissions
+from rest_framework.permissions import *
 from .models import Permission
 from . import register
+from django.contrib.auth.models import AnonymousUser
 
-class UserPermission(permissions.BasePermission):
+class UserPermission(permissions.IsAuthenticated):
 
     def has_permission(self, request, view):
+        # if user was not athenticated
+        if not super().has_permission(request, view):
+            return False
         if not hasattr(view, 'action'):
             return False
         return request.user.roles.filter(
