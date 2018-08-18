d3833bab621dfbc4064f7781414f523a0ccc917a
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 14:24:49 +0430

    setup added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='view_user_permission',
+    version='0.1',
+    description='checking view action permissions by user credinals',
+    author='Erfan Mehraban',
+    author_email='erfan.mehraban@gmail.com',
+    license='MIT',
+    packages=['view_user_permission', 'view_user_permission.migrations'],
+    install_requires=[
+        'cityhash==0.2.3.post9',
+        'Django==2.1',
+        'djangorestframework==3.8.2',
+    ],
+    include_package_data=True,
+    zip_safe=False)
