67a5a18fb7400edb760d4cc4bf04020420b37093
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 13:55:17 +0430

    if action not specified dont allow

--- view_user_permission/permissions.py
+++ view_user_permission/permissions.py
@@ -5,6 +5,8 @@ from . import register
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        if not hasattr(view, 'action'):
+            return False
         return request.user.roles.filter(
             permissions__name=Permission.get_name(register.get_view_name(view.__class__), view.action)
             ).exists()
