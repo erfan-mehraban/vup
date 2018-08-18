9d479af484bb221047f6408e6e45c45d6a805142
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-18 10:47:42 +0430

    (bug fix) order in choices

--- view_user_permission/models.py
+++ view_user_permission/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 action_map = (
-    ('retrieve','GET'),
-    ('create','POST'),
-    ('update','PUT'),
-    ('partial_update','PATCH'),
-    ('destroy','DELETE'),
+    ('GET', 'retrieve'),
+    ('POST', 'create'),
+    ('PUT', 'update'),
+    ('PATCH', 'partial update'),
+    ('DELETE', 'destroy'),
 )
 class Permission(models.Model):
     view = models.CharField(max_length=128)
     action = models.CharField(max_length=16,choices=action_map)
+
+    def __str__(self):
+        return self.view+' '+self.action
 
 class Group(models.Model):
     permissions = models.ManyToManyField(Permission, related_name="permissions")
