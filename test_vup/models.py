38d435c9317f9e3d13d61f5634e66b392e1ede6c
Erfan Mehraban <erfan.mehraban@gmail.com>
2018-08-20 15:28:29 +0430

    updating test app to comptible with new user-group relation

--- test_vup/models.py
+++ test_vup/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 
 class Number(models.Model):
     value = models.IntegerField()
+
+class User(AbstractUser):
+    roles = models.ManyToManyField('view_user_permission.Group', related_name="users")
